ad43f1593adccb55188157f826d125a74458a27d
Chris Gonzalez <christogonz92@gmail.com>
2024-08-02 15:53:18 +0200

    Initial Commit

---
+++ AnimatedStickyHeaderV2/AnimatedStickyHeaderV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedStickyHeaderV2App.swift
+//  AnimatedStickyHeaderV2
+//
+//  Created by Christopher Gonzalez on 2024-08-02.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedStickyHeaderV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
